e37fe8699806b26fa1dba2b5cc85f873bfb385c3
samuel ho <samuelho343@gmail.com>
2023-02-24 21:53:16 -0800

    app query client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { type AppType } from 'next/app';
