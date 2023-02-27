779edd3433e70d4c28d71c901ca20c1471b34c7c
samuel ho <samuelho343@gmail.com>
2023-02-26 22:15:31 -0800

    options

--- utils/gameOptions.js
+++ utils/gameOptions.js
@@ -1,7 +1,7 @@
 import getRandomSearch from './getRandom';
 
 export default function gameOptions(options) {
-  const gameOptions = options.albums.items.map((item) => {
+  const gameOptions = options?.albums.items.map((item) => {
     const gameOption = {
       artists: item.artists,
       external_urls: item.external_urls,
